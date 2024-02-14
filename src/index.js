d44012b721996cb4b2959e351c8ca6485a418812
pitaya <thanhlongprrt@gmail.com>
2024-02-15 00:09:24 +0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
+const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
 app.use(morgan('combined'));
 
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+
 app.get('/', (req, res) => {
-  res.send('123')
+  res.render('home')
 })
 
 // 127.0.0.1
