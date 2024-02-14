18f63370d6afad46a9f1c553b60bc2c794f743fa
pitaya <thanhlongprrt@gmail.com>
2024-02-15 01:00:56 +0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
-const express = require('express')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
 
 app.use(morgan('combined'));
 
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'));
 
 app.get('/', (req, res) => {
-  res.render('home')
-})
+  res.render('home');
+});
+
+app.get('/news', (req, res) => {
+  res.render('news');
+});
 
 // 127.0.0.1
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 })
