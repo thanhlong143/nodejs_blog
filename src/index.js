1116360c652af546fad8eab587c089ac14c71c7d
pitaya <thanhlongprrt@gmail.com>
2024-02-14 21:23:51 +0700

    message

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-app.get('/tin-tuc', (req, res) => {
-    var a = 1;
-    var b = 2;
-    var c = a + b;
-  res.send('Hello World!')
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+  res.send('123')
 })
 
 // 127.0.0.1
