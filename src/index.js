b88082d5b0919493d22e05492f1bff8c112b9f44
pitaya <thanhlongprrt@gmail.com>
2024-02-15 18:12:18 +0700

    Inital commit

--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', handlebars.engine({
@@ -22,7 +27,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/news', (req, res) => {
+  console.log(req.query.q)
   res.render('news');
+});
+
+app.get('/search', (req, res) => {
+  res.render('search');
+});
+
+app.post('/search', (req, res) => {
+  console.log(req.body);
+  res.send('');
 });
 
 // 127.0.0.1
