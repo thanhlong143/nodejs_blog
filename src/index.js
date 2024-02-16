b58373af67fae0b3f9aa729feffe5aabd9ada0ce
pitaya <thanhlongprrt@gmail.com>
2024-02-16 13:30:07 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -29,9 +29,9 @@ app.engine(
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
-route(      app);
+route(app);
 
 // 127.0.0.1
-                app.listen(port, () => {
-                    console.log(`Example app listening at http://localhost:${port}`);
-                });
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
