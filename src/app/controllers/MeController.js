137c0d22d1392270addf1d08c802db83a6af1f07
pitaya <thanhlongprrt@gmail.com>
2024-02-18 23:28:37 +0700

    commit

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,8 +4,9 @@ const { multipleMongooseToObject } = require('../../util/mongoose');
 class MeController {
     // [GET] /me/stored/courses
     storedCourses(req, res, next) {
-        Course.find({})
-            .then(courses => res.render('me/stored-courses', {
+        Promise.all([Course.find({}), Course.countDocumentsWithDeleted({deleted:true})])
+            .then(([courses, deletedCount]) => res.render('me/stored-courses', {
+                deletedCount,
                 courses: multipleMongooseToObject(courses),
             }))
             .catch(next);
@@ -17,7 +18,7 @@ class MeController {
             .then(courses => res.render('me/trash-courses', {
                 courses: multipleMongooseToObject(courses),
             }))
-            .catch(next);
+            .catch(() => {});
     };
 }
 
